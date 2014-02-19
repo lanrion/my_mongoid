b2d968b4bac166d7d554fb3a5ca57b0e2cd14032
lanrion <huaitao-deng@foxmail.com>
2014-02-19 17:43:36 +0800

    Model attributes and new

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,5 +1,51 @@
 require "my_mongoid/version"
+require 'pry-rails'
 
 module MyMongoid
-  # Your code goes here...
+
+  attr_accessor :models
+
+  module Document
+    attr_accessor :attributes
+
+    def self.included(klass)
+      MyMongoid.models = klass
+    end
+
+    def initialize(attrs = {})
+      raise ArgumentError if !attrs.is_a?(Hash)
+
+      attrs.each_pair do |attr_name, attr_value|
+        define_singleton_method(attr_name){attr_value}
+      end
+
+      @attributes = attrs
+    end
+
+    def read_attribute(attr_name)
+      @attributes.fetch(attr_name)
+    end
+
+    def write_attribute(attr_name, new_attr_value)
+      @attributes[attr_name]= new_attr_value
+    end
+
+    def new_record?
+      true
+    end
+
+    module ClassMethods
+
+    end
+  end
+
+  def self.models=(model)
+    @models ||= Array.new
+    @models << model
+  end
+
+  def self.models
+    @models
+  end
+
 end
