3e440b755341011c389d0e5ce8936235a7c022f2
lanrion <huaitao-deng@foxmail.com>
2014-02-23 12:40:56 +0800

    adde mongo sessions

---
+++ lib/my_mongoid/sessions.rb
@@ -0,0 +1,42 @@
+# encoding: utf-8
+module MyMongoid
+  # Defines behaviour for persistence operations that create new documents.
+  module Sessions
+    extend ActiveSupport::Concern
+
+    class << self
+
+      def default
+        @session ||= create_session
+      end
+    end
+
+    def collection
+      mongo_session[collection_name]
+    end
+
+    def mongo_session
+      Sessions.default
+    end
+
+    def collection_name
+      self.class.collection_name
+    end
+
+    private
+
+      def self.create_session(config={})
+        session = Moped::Session.new(config["host"]||["localhost:27017"])
+        session.use(config["database"]||"my_mongoid")
+        if authenticated?(config)
+          session.login(config[:username], config[:password])
+        end
+        session
+      end
+
+      def self.authenticated?(config)
+        config.has_key?(:username) && config.has_key?(:password)
+      end
+
+  end
+end
